a34115780e939b5f5bb540afa574578683ae415f
unknown <pierrekuh@yahoo.fr>
2023-11-26 15:17:58 +0100

    building music displayer

--- app.py
+++ app.py
@@ -1,14 +1,97 @@
 import spotify
+import security
+from urllib.parse import urlencode
+import requests
+import base64
 
-from flask import Flask, render_template, render_template_string, request
+
+from flask import Flask, render_template, render_template_string, request, redirect, session
+
+
+
+
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 
-# Routes
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+
+@app.route('/current_track')
+def current_track():
+    # access_token = session['access_token'] 
+    track_id = spotify.get_current_playing_track("BQBVVjuHilMo2cTUx-km1eRXlwwYreZNJsx2x_H5HToivINDTZE5-SFY5rBALvBVC1M7IIoT_wK_9O9b8MVXCocK1hLyYeUO_xM_1LGIxKlah1dNkfkRITguPKaW6roQfKBHbhezLNGy3enfqPY6qH4M11GVwVXlPHRxpRjGVVQksc5YWA1MY_KRAM9ux9sIgAoYXLWwYIm0dv-yZ3OBs0tOKfDG")  
+    print(track_id)
+    return track_id
+
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    state = security.get_random_string(16)
+    scope = 'user-read-private user-read-currently-playing user-read-playback-state'
+    params = urlencode({
+            'response_type': 'code',
+            'client_id': 'a47c281636bd4a8b907c14495533e837',
+            'scope': scope,
+            'redirect_uri': 'http://127.0.0.1:5000/callback',
+            'state': state
+        })
+    
+    redirect_url = 'https://accounts.spotify.com/authorize?' + params        
+    
+    return redirect(redirect_url)
+
+@app.route('/callback')
+def callback():
+    code = request.args.get('code')
+    state = request.args.get('state')
+
+    if state is None:
+        # Handle the state mismatch error
+        return redirect('/#' + urlencode({'error': 'state_mismatch'}))
+
+    # Your Spotify API credentials
+    client_id = 'a47c281636bd4a8b907c14495533e837'
+    client_secret = 'c86c9093985040b5a6253f3b66d1850a'
+    redirect_uri = 'http://127.0.0.1:5000/callback'
+
+    # Encode Client ID and Secret in Base64
+    client_credentials = f"{client_id}:{client_secret}"
+    client_credentials_b64 = base64.b64encode(client_credentials.encode()).decode()
+
+    # Headers
+    headers = {
+        'Authorization': f"Basic {client_credentials_b64}",
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    # POST data
+    post_data = {
+        'code': code,
+        'redirect_uri': redirect_uri,
+        'grant_type': 'authorization_code'
+    }
+
+    # Make the POST request
+    response = requests.post('https://accounts.spotify.com/api/token', headers=headers, data=post_data)
+
+    if response.status_code == 200:
+        # Extract and use the access token
+        access_token = response.json().get('access_token')
+        print("Access token : " + access_token)
+        session['access_token'] = access_token
+
+        return "Access token received."
+    else:
+        return "Error fetching access token."
+
 
 @app.route('/voting')
 def voting():
@@ -18,25 +101,69 @@ def voting():
 def search():
     query = request.form.get('query')
 
-    songs = spotify.search_song("BQD9x6uHPDMs6QolRfpRqZR_NKgr7RySjg-b8byVFIpjH-6a_qDX5YVb9KzFIPDMvvjkEMSLZtJFiCr-uTecdnHdV-wZLEuUteQzScZTISSEms4OEGM",query)
+    songs = spotify.search_song("BQCtDZW11VSCSLnz_I-t-znI7QL-he9_-OHIJvfc6ZwOEbwjOvuNh1eXPeZkwaKH4eacqA4IPdTxGN2QvxO4QCK4yy_hw31k4mqq_UCTRaIY43e7EFQ", query)
+    
     search_results_template = """
+    <style>
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #f8f8f8;
+            border: 1px solid #ddd;
+            border-radius: 5px;
+        }
+        h2 {
+            font-size: 24px;
+            margin-bottom: 10px;
+        }
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+        li {
+            margin-bottom: 20px;
+        }
+        h3 {
+            font-size: 20px;
+            margin-bottom: 5px;
+        }
+        p {
+            margin: 0;
+        }
+        .vote-button {
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            padding: 5px 10px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 12px;
+            margin-top: 5px;
+            cursor: pointer;
+            border-radius: 3px;
+        }
+    </style>
+    <div class="container">
         <h2>Search Results</h2>
         <ul>
             {% for song in songs %}
             <li>
                 <h3>{{ song['name'] }}</h3>
-                <p>Artist: {{ song['artist'] }}</p>
-                <p>Album: {{ song['album'] }}</p>
-                <audio controls>
-                    <source src="{{ song['preview_url'] }}" type="audio/mpeg">
-                </audio>
+                <p>Artist: {{ song['album']['artists'][0]['name'] }}</p>
+
+                <button class="vote-button">Vote</button>
             </li>
             {% endfor %}
         </ul>
+    </div>
     """
+    
     rendered_results = render_template_string(search_results_template, songs=songs)
 
     return rendered_results
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- spotify.py
+++ spotify.py
@@ -1,5 +1,9 @@
 import requests
 
+
+
+
+
 def get_access_token(id,user_secret):
     url = 'https://accounts.spotify.com/api/token'
     headers = {
@@ -15,9 +19,6 @@ def get_access_token(id,user_secret):
     access_token = response.json()['access_token']
 
     return response.json()
-
-
-
 
 
 def search_song(access_token,query):
@@ -48,6 +49,31 @@ def search_song(access_token,query):
 #         print(f"Preview URL: {track['preview_url']}")
 #         print("-----")
 
- 
-#print(get_access_token("a47c281636bd4a8b907c14495533e837","c86c9093985040b5a6253f3b66d1850a"))
+def get_current_playing_track(access_token):
+    url = "https://api.spotify.com/v1/me/player/currently-playing"
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        track_id = data['item']['id']  # Extract the track ID
+        return track_id
+    else:
+        # Handle errors or no current playing track
+        return response
+
+# Usage
+access_token = "your_access_token"  # Replace with your actual access token
+current_track_id = get_current_playing_track(access_token)
+if current_track_id:
+    print(f"Current playing track ID: {current_track_id}")
+else:
+    print("No track is currently playing or unable to fetch the track.")
+
+
+
+# print(get_current_song("BQCwKHRKbolk8l74UE5o6fXTpbCifFZHiFbwOx18aOx-mp2bmiXVvM5rzkoXOXABSiUTPT_5LrHYtV2_F7PbCM-rERVDzfOjThWclkH7D3Md8RXOyVo"))
+# print(get_access_token("a47c281636bd4a8b907c14495533e837","c86c9093985040b5a6253f3b66d1850a"))
 # print(search_song("BQD9x6uHPDMs6QolRfpRqZR_NKgr7RySjg-b8byVFIpjH-6a_qDX5YVb9KzFIPDMvvjkEMSLZtJFiCr-uTecdnHdV-wZLEuUteQzScZTISSEms4OEGM", "with or without u"))
+
